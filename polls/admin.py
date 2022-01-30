926cf3cc1345e01c5bdbd7a0ec8374a84c25013f
Lucas Idalino <93747485+LucasIdalino@users.noreply.github.com>
2022-01-30 14:07:54 -0300

    primeiro commit

---
+++ polls/admin.py
@@ -0,0 +1,24 @@
+from django.contrib import admin
+from .models import Question, Choice
+
+
+class ChoiceInLine(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {'fields': ['question_text']}),
+        ('Date Information', {'fields': ['pub_date'],
+                              'classes': ['collapse']}),
+        ]
+    inlines = [ChoiceInLine]
+
+    search_fields = ['question_text']
+    list_filter = ['pub_date']
+    list_display = ('question_text', 'pub_date', 'was_published_recent')
+
+
+admin.site.register(Question, QuestionAdmin)
+
